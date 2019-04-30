701791e0a49f971bd6519d2b7bed96b2d6f383ae
chris aquino <chris@radishmouse.com>
2019-04-30 15:24:14 -0400

    begins usage of react router

--- src/App.js
+++ src/App.js
@@ -2,23 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Home from './Home';
+import About from './About';
+import Cats from './Cats';
+
+import {
+  Link,
+  Route
+} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Link is what you use
+          instead of an anchor!
+       */}
+      <Link to="/">Home</Link>
+      <Link to="/cats">Cats</Link>
+      <Link to="/about">About</Link>
+
+      {/* <Home /> */}
+      <Route exact path="/" component={Home} />
+      {/* <Cats /> */}
+      <Route path="/cats" component={Cats} />
+      {/* <About /> */}
+      <Route path="/about" component={About} />
     </div>
   );
 }
